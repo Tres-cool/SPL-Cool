31c864a9996e1b760627eeb2174a01792dbc99f7
MohammadBlockstar <mohammad@blockstars.com.au>
2024-02-14 08:58:03 +0330

    DONE

--- programs/solana-carbon-contract/src/lib.rs
+++ programs/solana-carbon-contract/src/lib.rs
@@ -1,26 +1,58 @@
 use anchor_lang::prelude::*;
+use std::mem::size_of;
 // use std::{str::FromStr};
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("EEETpgqxVBBW7MgmhXU1iH763C2xBpBV7VmnLxjFx2Ga");
 
 #[program]
 pub mod solana_carbon_contract {
     use super::*;
 
-    pub fn init_config(ctx: Context<InitConfig>) -> Result<()> {
-        ctx.accounts.config.authority = ctx.accounts.authority.key();
-        ctx.accounts.config.bump = *ctx.bumps.get("config").unwrap();
+    pub fn init_config(ctx: Context<InitConfig>, init_data: InitConfigData) -> Result<()> {
+        // Extract user_id from the passed instruction data
+        let user_id = init_data.owner;
+        // Generate the PDA using the CARBON_CONFIG seed and user_id
+        let seeds = &[CARBON_CONFIG, user_id.as_ref()];
+        let bump = Pubkey::find_program_address(seeds, ctx.program_id).1;
+        // Initialize the config account
+        let config_account = &mut ctx.accounts.config;
+        config_account.authority = ctx.accounts.authority.key();
+        // config_account.authorised = authorised_id.into();
+        config_account.bump = bump; // Use the dynamically generated bump
+
         Ok(())
     }
 
-    pub fn update_config(ctx: Context<UpdateConfig>) -> Result<()> {
+    pub fn init_authorised(ctx: Context<InitAuth>, init_data:InitAuthData) -> Result<()> {
+        // Extract authorised_id from the passed instruction data
+        let authorised_id = init_data.authorised;
+        let id = init_data.id;
+        // Generate the PDA using the CARBON_AUTHORISED seed and user_id
+        let authorised_seeds = &[CARBON_AUTHORISED, id.as_ref()];
+        let authorised_bump = Pubkey::find_program_address(authorised_seeds, ctx.program_id).1;
+        // Initialize the authorised account
+        let carbon_delegate = &mut ctx.accounts.carbon_auth;
+        carbon_delegate.authority = authorised_id.into();
+        carbon_delegate.bump = authorised_bump; // Use the dynamically generated bump
+    
+        Ok(())
+    }
+
+    pub fn update_config(ctx: Context<UpdateConfig>, update_data: UpdateConfigData) -> Result<()> {
         ctx.accounts.config.authority = ctx.accounts.new_authority.key();
         Ok(())
     }
 
-    pub fn withdraw(ctx: Context<Withdraw>) -> Result<()> {
+
+    pub fn withdraw(ctx: Context<WithdrawAuthorised>, id:String) -> Result<()> {
         require!(ctx.accounts.delegate_pda.lamports() > 0, CarbonError::InsuffucientFund);
-        // require!(ctx.accounts.authority.key() == Pubkey::from_str(ADMIN_AUTHORITY).unwrap(), CarbonError::InvalidAuthority);
+		let rent_exempt_balance = ctx.accounts.rent.minimum_balance(ctx.accounts.delegate_pda.data_len());
+		
+		// Ensure that delegate_pda has more than the rent-exempt balance.
+		require!(ctx.accounts.delegate_pda.lamports() > rent_exempt_balance, CarbonError::InsuffucientFund);
+	
+		// Calculate the amount that can be safely withdrawn without going below the rent-exempt balance.
+		let withdrawable_amount = ctx.accounts.delegate_pda.lamports() - rent_exempt_balance;
         let seeds = &[CARBON_DELEGATE];
         let (_delegate_pda_key, bump) = Pubkey::find_program_address(seeds, &crate::ID);
 
@@ -31,7 +63,7 @@ pub mod solana_carbon_contract {
         let signer = &[&signer_seeds[..]];
 
         anchor_lang::solana_program::program::invoke_signed(
-            &anchor_lang::solana_program::system_instruction::transfer(ctx.accounts.delegate_pda.key, ctx.accounts.authority.key, ctx.accounts.delegate_pda.lamports()),
+            &anchor_lang::solana_program::system_instruction::transfer(ctx.accounts.delegate_pda.key, ctx.accounts.authority.key, withdrawable_amount),
             &[
                 ctx.accounts.delegate_pda.to_account_info(),
                 ctx.accounts.authority.to_account_info(),
@@ -41,6 +73,7 @@ pub mod solana_carbon_contract {
         )?;
         Ok(())
     }
+
 }
 
 #[derive(Accounts)]
@@ -57,10 +90,49 @@ pub struct InitConfig<'info> {
         space = std::mem::size_of::<Config>() + 8,
     )]
     pub config: Box<Account<'info, Config>>,
+    pub system_program: Program<'info, System>,
+    pub rent: Sysvar<'info, Rent>,
+}
+
+#[derive(Accounts)]
+#[instruction(init_data:InitAuthData)]
+pub struct InitAuth<'info> {
+    #[account(mut)]
+    pub authority: Signer<'info>,
+
+    // Include the existing Config account to verify its authority
+    #[account(
+        seeds = [CARBON_CONFIG],
+        bump,
+        constraint = config.authority == authority.key() @ CarbonError::InvalidAuthority,
+    )]
+    pub config: Account<'info, Config>,
+
+    #[account(
+        init,
+        seeds = [CARBON_AUTHORISED, init_data.id.as_ref()], // dynamic seed based on unique_id
+        bump,
+        payer = authority,
+        space = 8 + std::mem::size_of::<Config>(), // Adjusted space calculation
+    )]
+    pub carbon_auth: Box<Account<'info, Config>>,
 
     pub system_program: Program<'info, System>,
     pub rent: Sysvar<'info, Rent>,
 }
+
+
+#[derive(AnchorSerialize, AnchorDeserialize)]
+pub struct InitConfigData {
+    pub owner: [u8; 32], // Adjust the size as needed
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize)]
+pub struct InitAuthData {
+    pub authorised: [u8; 32],
+    pub id: String // Adjust the size as needed
+}
+
 
 #[derive(Accounts)]
 #[instruction()]
@@ -70,7 +142,7 @@ pub struct UpdateConfig<'info> {
 
     #[account()]
     /// CHECK: we read this key only
-    pub new_authority: UncheckedAccount<'info>,
+    pub new_authority: Signer<'info>,
 
     #[account(
         mut,
@@ -84,10 +156,16 @@ pub struct UpdateConfig<'info> {
     pub rent: Sysvar<'info, Rent>,
 }
 
+#[derive(AnchorSerialize, AnchorDeserialize)]
+pub struct UpdateConfigData {
+    pub user_id: [u8; 32], // Adjust the size as needed
+}
+
+
 #[derive(Accounts)]
-#[instruction()]
-pub struct Withdraw<'info> {
-    #[account(mut,)]
+#[instruction(id:String)]
+pub struct WithdrawAuthorised<'info> {
+    #[account(mut)]
     pub authority: Signer<'info>,
     
     /// CHECK: we read this key only
@@ -100,13 +178,15 @@ pub struct Withdraw<'info> {
 
     #[account(
         mut,
-        seeds = [CARBON_CONFIG],
+        seeds = [CARBON_AUTHORISED,id.as_ref()],
         bump,
-        constraint = config.authority == authority.key() @ CarbonError::InvalidAuthority,
+        // constraint = carbon_auth.authority == authority.key() @ CarbonError::InvalidAuthority,
     )]
-    pub config: Box<Account<'info, Config>>,
+    pub carbon_auth: Box<Account<'info, Config>>,
 
     pub system_program: Program<'info, System>,
+
+    pub rent: Sysvar<'info, Rent>,
 }
 
 #[account]
@@ -116,6 +196,7 @@ pub struct Config {
     pub authority: Pubkey,
     pub reserved: [u128; 10],
 }
+
 
 #[error_code]
 pub enum CarbonError {
@@ -128,4 +209,4 @@ pub enum CarbonError {
 
 pub const CARBON_CONFIG: &[u8] = b"CARBON_CONFIG";
 pub const CARBON_DELEGATE: &[u8] = b"CARBON_DELEGATE";
-// pub const ADMIN_AUTHORITY: &str = "";
+pub const CARBON_AUTHORISED: &[u8] = b"CARBON_AUTHORISED";
--- tests/solana-carbon-contract.ts
+++ tests/solana-carbon-contract.ts
@@ -1,16 +1,271 @@
-import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
-import { SolanaCarbonContract } from "../target/types/solana_carbon_contract";
+import * as anchor from '@coral-xyz/anchor';
+import { PublicKey, SystemProgram, SYSVAR_RENT_PUBKEY, Keypair } from '@solana/web3.js';
+// import { SolanaCarbonContract } from '../target/types/solana_carbon_contract';
+import fs from 'fs';
+import * as assert from 'assert';
 
-describe("solana-carbon-contract", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+async function getBalance(publicKey: PublicKey, provider: anchor.AnchorProvider): Promise<number> {
+  return await provider.connection.getBalance(publicKey);
+}
 
-  const program = anchor.workspace.SolanaCarbonContract as Program<SolanaCarbonContract>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+// describe('Solana Carbon Contract', () => {
+//   anchor.setProvider(anchor.AnchorProvider.env());
+
+//   it('Should update the config', async () => {
+
+//     const provider = anchor.AnchorProvider.env();
+//     anchor.setProvider(provider);   
+
+//     const program = anchor.workspace.SolanaCarbonContract;
+//     const payer = program.provider.wallet.payer;
+
+//     // Initialize the config account
+//     const configKeypair = Keypair.generate();
+//     const [configPda, configBump] = await PublicKey.findProgramAddress(
+//       [Buffer.from(anchor.utils.bytes.utf8.encode("CARBON_CONFIG"))],
+//       program.programId
+//     );
+
+//     // console.log("payer isss: ", payer.publicKey)
+//     // console.log("configPda is :", configPda)
+//     // console.log(program.programId)
+//     // console.log(SystemProgram.programId)
+
+//     // Example user_id as a 32-byte array
+//     const userId = new anchor.web3.PublicKey(payer.publicKey).toBytes();
+//     const tx = await program.rpc.initConfig(
+//       {
+//         userId, // Pass the user_id as part of the instruction data
+//       },{
+//       accounts: {
+//         authority: payer.publicKey,
+//         config: configPda,
+//         systemProgram: SystemProgram.programId,
+//         rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+//       },
+//       signers: [payer],
+//     });
+
+//     // Confirm the transaction.
+//     await program.provider.connection.confirmTransaction(tx, 'confirmed');
+
+//     // New authority
+//     const newAuthority = Keypair.generate();
+//     console.log(newAuthority.publicKey)
+
+//     // Check the signer's balance before withdrawal
+//     const initialSignerBalance = await getBalance(newAuthority.publicKey, provider);
+//     console.log("Initial newAuthority Balance:", initialSignerBalance);
+
+//     // Send SOL to delegate_pda
+//     const lamportsToSend = 1000000; // Number of lamports to send (1 SOL = 1,000,000 lamports)
+//     const transferTx = new anchor.web3.Transaction().add(
+//       anchor.web3.SystemProgram.transfer({
+//         fromPubkey: payer.publicKey,
+//         toPubkey: newAuthority.publicKey,
+//         lamports: lamportsToSend,
+//       })
+//     );
+//     // Sign and send the transaction
+//     await provider.sendAndConfirm(transferTx, [payer]);
+
+//       // Check the signer's balance before withdrawal
+//       const afterSignerBalance = await getBalance(newAuthority.publicKey, provider);
+//       console.log("AfterPay newAuthority Balance:", afterSignerBalance);
+
+//     // Update the config
+//     const updateTx = await program.rpc.updateConfig(      {
+//       userId, // Pass the user_id as part of the instruction data
+//       },{
+//       accounts: {
+//         authority: payer.publicKey,
+//         newAuthority: newAuthority.publicKey,
+//         config: configPda,
+//         systemProgram: SystemProgram.programId,
+//         rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+//       },
+//       signers: [payer, newAuthority],
+//     });
+
+//     await program.provider.connection.confirmTransaction(updateTx, 'confirmed');
+
+//     // // Fetch the updated config account
+//     const updatedConfig = await program.account.config.fetch(configPda);
+
+//     // Assert the new authority
+//     assert.equal(updatedConfig.authority.toString(), newAuthority.publicKey.toString());
+
+//   });
+// });
+
+it('Should send SOL to the delegate_pda and withdraw', async () => {
+  const program = anchor.workspace.SolanaCarbonContract;
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);  
+  const payer = program.provider.wallet.payer;
+
+  const [configPda, configBump] = await PublicKey.findProgramAddress(
+    [Buffer.from(anchor.utils.bytes.utf8.encode("CARBON_CONFIG"))],
+    program.programId
+  );
+
+  const owner = new anchor.web3.PublicKey(payer.publicKey).toBytes();
+  const tx_Owner_config = await program.rpc.initConfig(
+    {
+      owner, // Pass the user_id as part of the instruction data
+    },{
+    accounts: {
+      authority: payer.publicKey,
+      config: configPda,
+      systemProgram: SystemProgram.programId,
+      rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+    },
+    signers: [payer],
   });
+    // Confirm the transaction.
+  await program.provider.connection.confirmTransaction(tx_Owner_config, 'confirmed');
+
+
+  let id = "1";
+  const [authPda, authBump] = await PublicKey.findProgramAddress(
+    [Buffer.from(anchor.utils.bytes.utf8.encode("CARBON_AUTHORISED"+id))],
+    program.programId
+  );
+
+  let authorised1 = new anchor.web3.PublicKey(payer.publicKey);
+  // console.log("This is the authrosied1", authorised1)
+
+
+  const tx_auth_1 = await program.rpc.initAuthorised(
+    {
+      authorised1,
+      id
+    },
+    {
+    accounts: {
+      authority: payer.publicKey,
+      config:configPda,
+      carbonAuth: authPda,
+      systemProgram: SystemProgram.programId,
+      rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+    },
+    signers: [payer],
+  });
+
+      // Confirm the transaction.
+    await program.provider.connection.confirmTransaction(tx_auth_1, 'confirmed');
+  
+  
+    id = "12";
+    const [authPda2, authBump2] = await PublicKey.findProgramAddress(
+      [Buffer.from(anchor.utils.bytes.utf8.encode("CARBON_AUTHORISED"+id))],
+      program.programId
+    );
+
+    const newAccount = Keypair.generate();
+    // console.log(newAccount.publicKey)
+    let authorised2 = newAccount.publicKey.toBytes();
+    // console.log("This is the authrosied22222", authorised2)
+
+    const tx_auth_2 = await program.rpc.initAuthorised(
+      {
+        authorised2,
+        id  
+      },
+      {
+      accounts: {
+        authority: payer.publicKey,
+        config:configPda,
+        carbonAuth: authPda2,
+        systemProgram: SystemProgram.programId,
+        rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+      },
+      signers: [payer],
+    });
+  
+  // Confirm the transaction.
+  await program.provider.connection.confirmTransaction(tx_auth_2, 'confirmed');
+  
+
+  // Derive the delegate_pda address
+  const [delegatePda, delegateBump] = await PublicKey.findProgramAddress(
+    [Buffer.from(anchor.utils.bytes.utf8.encode("CARBON_DELEGATE"))],
+    program.programId
+  );
+
+  // Check the signer's and delegate account balance before withdrawal
+  const initialSignerBalance = await getBalance(payer.publicKey, provider);
+  console.log("Initial Payer Balance:", initialSignerBalance);
+  const initialDelegateBalance = await getBalance(delegatePda, provider);
+  console.log("Initial Delegate Balance:", initialDelegateBalance);
+  const initialNewAccountBalance = await getBalance(newAccount.publicKey, provider);
+  console.log("Initial New Account Balance:", initialNewAccountBalance);
+  ////////////////////////////////////
+
+  // Send SOL to delegate_pda
+  const lamportsToSend = 1000000000; // Number of lamports to send (1 SOL = 1,000,000 lamports)
+  const transferTx = new anchor.web3.Transaction().add(
+    anchor.web3.SystemProgram.transfer({
+      fromPubkey: payer.publicKey,
+      toPubkey: delegatePda,
+      lamports: lamportsToSend,
+    })
+  );
+  await provider.sendAndConfirm(transferTx, [payer]);
+
+  const lamportsToSendNew = 1000000000; // Number of lamports to send (1 SOL = 1,000,000 lamports)
+  const transferTxNew = new anchor.web3.Transaction().add(
+    anchor.web3.SystemProgram.transfer({
+      fromPubkey: payer.publicKey,
+      toPubkey: newAccount.publicKey,
+      lamports: lamportsToSendNew,
+    })
+  );
+  // Sign and send the transaction
+  await provider.sendAndConfirm(transferTxNew, [payer]);
+
+    // Check the signer's and delegate balance after paying the delegate
+    const afterSignerBalance = await getBalance(payer.publicKey, provider);
+    console.log("AfterPay signer Balance:", afterSignerBalance);
+    const afterDelegateBalance = await getBalance(delegatePda, provider);
+    console.log("AfterPay Delegate Balance:", afterDelegateBalance);
+    const afterNewAccountBalance = await getBalance(newAccount.publicKey, provider);
+    console.log("AfterPay New account Balance:", afterNewAccountBalance);
+    ///////////////////////////////////
+
+    id ="12"
+    let authorised = new anchor.web3.PublicKey(payer.publicKey);
+    // console.log("==================== all arguments for withdraw");
+    // console.log("authorised1:", authorised1);
+    // console.log("id:", id);
+    // console.log("authority:", payer.publicKey);
+    // console.log("delegatePda:", delegatePda);
+    // console.log("carbonAuth:", authPda);
+    // console.log("===============================================");
+  // Withdraw SOL from delegate_pda
+  const withdrawTx = await program.rpc.withdraw(
+    id  
+    ,{
+    accounts: {
+      authority: newAccount.publicKey,
+      delegatePda: delegatePda,
+      carbonAuth: authPda2, // Make sure this is the correct config PDA
+      systemProgram: SystemProgram.programId,
+      rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+    },
+    signers: [newAccount],
+  });
+    // Confirm the transaction.
+  await program.provider.connection.confirmTransaction(withdrawTx, 'confirmed');
+
+    // Check the signer's and delegate balance after paying the delegate
+    const afterWithSignerBalance = await getBalance(payer.publicKey, provider);
+    console.log("After withdraw signer Balance:", afterWithSignerBalance);
+    const afterWithDelegateBalance = await getBalance(delegatePda, provider);
+    console.log("After withdraw Delegate Balance:", afterWithDelegateBalance);
+    const afterWithNewAccountBalance = await getBalance(newAccount.publicKey, provider);
+    console.log("After withdraw  New account Balance:", afterWithNewAccountBalance);
+    ///////////////////////////////////
+
 });
